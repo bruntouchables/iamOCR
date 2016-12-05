5b0eaec6a2834b00ab74bde455f6926d58959bae
kantuni <henrikh.kantuni@gmail.com>
2016-12-05 03:50:45 -0500

    Install OpenCV

--- preprocessing.py
+++ preprocessing.py
@@ -1,2 +1,15 @@
 """ Created by Henrikh Kantuni on 12/4/16 """
 
+from matplotlib import pyplot as plt
+import numpy as np
+import cv2 as cv
+
+# read an image
+test_image = cv.imread('test.jpg', 0)
+
+# show an image
+cv.imshow('Original', test_image)
+cv.waitKey(0)
+
+# clean up
+cv.destroyAllWindows()
